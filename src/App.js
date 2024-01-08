c7ce7c064a08b0f023580ffbe5faea787c92bb25
Meiwen Dong <richard@Meiwens-iMac.local>
2024-01-08 18:04:07 -0500

    add open graph and logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
-export default App;
+function Header() {
+  return (
+    <header>
+    </header>
+  )
+}
+
+function Nav() {
+  return (
+    <nav>
+      <Logo/>
+      <ul>
+        <li>
+          <a></a>
+        </li>
+        <li>
+          <a></a>
+        </li>
+        <li>
+          <a></a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+function Main() {
+  return (
+    <main></main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer></footer>
+  )
+}
+
+function Logo() {
+  return (
+    <img></img>
+  )
+}
